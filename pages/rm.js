77c37c3de84a7bf6dc625b9b4178b8363ef2bac5
DevWithEasy <devwitheasy@gmail.com>
2022-12-26 17:47:18 +0600

    number bug fixed

--- pages/rm.js
+++ pages/rm.js
@@ -84,13 +84,13 @@ export default function Raw({sections}) {
                         </div>
               <div className='space-y-2'>
                 {
-                  product?.ingredients && product.ingredients.map((ingredient)=><div key={ingredient.code_name} className="w-full flex justify-between py-2 border-b items-center rounded hover:bg-gray-100 transition-all duration-300 hover:scale-[1.02]">
+                  product?.ingredients && product.ingredients.map((ingredient)=><div key={ingredient.code_name} className="w-full flex justify-between py-2 border-b items-center rounded hover:bg-gray-500 hover:text-white transition-all duration-300 hover:scale-[1.02]">
                   <label htmlFor="" className='w-3/4 pl-2'>{ingredient.name}</label>
                     <div className='w-1/4 text-center'>
                     {/* {parseFloat(Number(ingredient.quantity) * Number(batch)).toFixed(2)} */}
                       {/* <input value={Number(ingredient.quantity) * Number(batch)} disabled></input> */}
                       {
-                        parseFloat(Number(ingredient.quantity) * Number(batch)) == 0 ? "-" : parseFloat(Number(ingredient.quantity) * Number(batch).toFixed(2))
+                        parseFloat(Number(ingredient.quantity) * Number(batch)) == 0 ? "-" : parseFloat(Number(ingredient.quantity) * Number(batch)).toFixed(2)
                       }
                     </div>
               </div>)
