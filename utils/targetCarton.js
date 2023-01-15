6153c83e8703a0a9c98fc9d64b915abdffbaa700
DevWithEasy <devwitheasy@gmail.com>
2023-01-15 22:27:27 +0600

    laschcha demand

--- utils/targetCarton.js
+++ utils/targetCarton.js
@@ -1,3 +1,3 @@
-export default function targetCarton(targetCarton){
+export default function totalTargetCartonWithExtra(targetCarton){
     return Number((Number(targetCarton)+(Number(targetCarton)*20)/100))
 }
