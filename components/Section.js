b8802cd49e42174320fc2ead182f76b4ab0bf7c8
DevWithEasy <devwitheasy@gmail.com>
2023-02-01 00:52:53 +0600

    log redirect link

--- components/Section.js
+++ components/Section.js
@@ -5,7 +5,7 @@ import { useDispatch } from "react-redux";
 import { logout } from "../features/slice/userSlice";
 
 export default function Section(props){
-  const {username,section} = props;
+  const {name,username,section} = props;
   const dispatch = useDispatch()
   const router = useRouter()
   const logoutUser =()=>{
@@ -16,18 +16,21 @@ export default function Section(props){
         <div className=''>
           <h1 className="relative text-center p-2 font-bold text-2xl bg-gray-100">
             {section}
-            <AiOutlineLogout onClick={()=>logoutUser()} className="absolute top-3 right-3 cursor-pointer hover:text-red-500"/>
+            <div className="absolute top-3 right-3 flex items-center space-x-2">
+              <span className="text-xs">Hello {name}</span>
+              <AiOutlineLogout onClick={()=>logoutUser()} className="cursor-pointer hover:text-red-500"/>
+            </div>
           </h1>
-          <Link href={`/recipe/${username}`}>
+          <Link href={`/recipe/${section.toLowerCase()}`}>
             <a className='block p-2 border-b text-blue-500 text-xl'>Recipe</a>
           </Link>
-          <Link href={`/consumption/rm/${username}`}>
+          <Link href={`/consumption/rm/${section.toLowerCase()}`}>
             <a className='block p-2 border-b text-blue-500 text-xl'>RM Calcutation</a>
           </Link>
-          <Link href={`/consumption/pm/${username}`}>
+          <Link href={`/consumption/pm/${section.toLowerCase()}`}>
             <a className='block p-2 border-b text-blue-500 text-xl'>PM Calcutation</a>
           </Link>
-          <Link href={`/monthly_demand/${username}`}>
+          <Link href={`/monthly_demand/${section.toLowerCase()}`}>
             <a className='block p-2 border-b text-blue-500 text-xl'>Monthly Demand</a>
           </Link>
         </div>
