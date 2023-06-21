319b05d872aeac5f23f0a88ea04085864913e5cc
DevWithEasy <devwitheasy@gmail.com>
2023-06-21 08:37:49 +0600

    target caton component refractor

--- pages/admin.js
+++ pages/admin.js
@@ -1,6 +1,8 @@
 import { useRouter } from 'next/router';
 import DashBoard from '../components/dashboard/DashBoard';
 import useUserStore from "../features/userStore";
+import Image from 'next/image';
+import not_allow from '../public/not_allow.svg'
 
 
 export default function Admin({}){
@@ -9,12 +11,11 @@ export default function Admin({}){
     
     if(user.section != 'Admin'){
         return(
-            <div>
-                <p>You are not allowed to</p>
+            <div className='text-center text-red-500 text-2xl'>
+                <Image src={not_allow} alt='not_allow' height={300} width={300}/>
+                <p>You are not allowed.</p>
             </div>
         )
-    }else if(!user.name){
-        return router.push('/')
     }else{
         return <DashBoard/>
     }
