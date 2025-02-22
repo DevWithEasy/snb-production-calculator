668a034f5c3c6fca7f7949e399e2d84fd9d0ba1a
DevWithEasy <devwitheasy@gmail.com>
2025-02-22 18:31:25 +0600

    api change

--- utils/v2/appscript_api_url.js
+++ utils/v2/appscript_api_url.js
@@ -1,4 +1,4 @@
-const deploy_id = 'https://script.google.com/macros/s/AKfycbwJeQpMwXawxp6odaIWvLVJgnkOzXxll0tIWt3_9EQpU45YaOapOIjoBGxGu-hfO5kR/exec?'
+const deploy_id = 'https://script.google.com/macros/s/AKfycbzMYES5UNLiKmw-7XvRa7p95o1zpuD0jppjFvbGagL3YCx88Wsda6HVVdm7lOmKsYUs/exec?'
 
 const getAPI=()=>{
     return deploy_id
